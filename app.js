f2a0af99242213a4d5229ed2ad5a0996229d7c9f
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2020-11-27 02:29:42 +0900

    20201127

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var bodyParser = require("body-parser");
 const { request } = require("https");
 var cookieParser = require("cookie-parser");
 /*-----------------------------------------*/
-var userLogin = require('./router/login');
-var userSignup = require('./router/signin');
-var userInsert = require('./router/insert');
-const deleteItem = require('./router/delete');
-const changePw = require('./router/changePw');
+var userLogin = require("./router/login");
+var userSignup = require("./router/signin");
+var userInsert = require("./router/insert");
+const deleteItem = require("./router/delete");
+const changePw = require("./router/changePw");
 /*-----------------------------------------*/
-const mysqli = require('./database/database');
-const ses = require('./config/config');
+const mysqli = require("./database/database");
+const ses = require("./config/config");
 /*-----------------------------------------*/
 
 //Server를 생성
@@ -25,11 +25,11 @@ app.use(
 //미들웨어 추가
 app.use(express.static("Web/Login_v3"));
 app.use(ses); //session정보 (config/config.js)
-app.use('/',userLogin);
-app.use('/signin',userSignup);
-app.use('/insert',userInsert);
-app.use('/delete',deleteItem); 
-app.use('/changepw',changePw); 
+app.use("/", userLogin);
+app.use("/signin", userSignup);
+app.use("/insert", userInsert);
+app.use("/delete", deleteItem);
+app.use("/changepw", changePw);
 
 //서버를 실행
 app.listen(52273, function () {
@@ -39,17 +39,21 @@ app.listen(52273, function () {
 app.get("/list", function (req, res) {
   //파일 읽기
   var name = req.session.name;
+  var sql =
+    "SELECT s.num, s.user_id, m.LOCATION, s.roomno, date_format(time,'%Y-%m-%d %H:%i:%s')as time, date_format((date_add(time,interval 45 minute)),'%Y-%m-%d %H:%i:%s') as endtime from state s, machine m where s.num = m.MACHINE_ID";
+  var del =
+    "DELETE FROM STATE WHERE time < DATE_ADD(now(), INTERVAL -1 MINUTE);";
   if (!req.session) return res.render("login");
   if (req.session.authenticate) {
     fs.readFile("./Web/list.html", "utf8", function (error, data) {
-      mysqli.query(
-        "SELECT s.num, s.user_id, m.LOCATION, s.roomno, date_format(time,'%Y-%m-%d %H:%i:%s')as time, date_format((date_add(time,interval 45 minute)),'%Y-%m-%d %H:%i:%s') as endtime from state s, machine m where s.num = m.MACHINE_ID",
-        function (error, results) {
+      mysqli.query(sql, function (error, results) {
+        //response
+        mysqli.query(del, function () {
           //response
           res.send(ejs.render(data, { data: results, name: name }));
           console.log("data: ", results);
-        }
-      );
+        });
+      });
     });
   }
 });
