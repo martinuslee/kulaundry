5a3043f6f35b65fdb3105a01f858dfa3fbf2aac6
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-05-02 00:00:59 +0900

    createPool

--- database/database.js
+++ database/database.js
@@ -1,10 +1,12 @@
 var mysql = require("mysql2");
 var db_config = require("../config/db_config.json");
-var client = mysql.createConnection({
+
+var client = mysql.createPool({
   host: db_config.host,
   user: db_config.user,
   password: db_config.password,
   database: db_config.database,
 });
+
 
 module.exports = client;
