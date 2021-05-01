241759925f40ce67e4269fae0102c3dcf99d19c3
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-05-01 22:03:29 +0900

    db connection lost handle

--- database/database.js
+++ database/database.js
@@ -8,14 +8,14 @@ var client = mysql.createConnection({
 });
 
 function handleDisconnect() {
-  db.connect(function (err) {
+  client.connect(function (err) {
     if (err) {
       console.log("error when connecting to db:", err);
       setTimeout(handleDisconnect, 2000);
     }
   });
 
-  db.on("error", function (err) {
+  client.on("error", function (err) {
     console.log("db error", err);
     if (err.code === "PROTOCOL_CONNECTION_LOST") {
       return handleDisconnect();
