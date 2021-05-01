2de07004bde4c4e521b90afcb1a2fd0ddfe3b798
supreme034@korea.ac.kr <supreme034@korea.ac.kr>
2021-05-01 22:01:27 +0900

    db connection lost handle

--- database/database.js
+++ database/database.js
@@ -1,10 +1,30 @@
 var mysql = require("mysql2");
-var db_config  = require('../config/db_config.json');
+var db_config = require("../config/db_config.json");
 var client = mysql.createConnection({
-    host: db_config.host,
-    user: db_config.user,
-    password: db_config.password,
-    database: db_config.database
+  host: db_config.host,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+});
+
+function handleDisconnect() {
+  db.connect(function (err) {
+    if (err) {
+      console.log("error when connecting to db:", err);
+      setTimeout(handleDisconnect, 2000);
+    }
   });
 
-  module.exports = client;
+  db.on("error", function (err) {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      return handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = client;
